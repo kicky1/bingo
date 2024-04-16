e30246083db45ffa2f432ebef8d0f4a3bae0546b
unknown <krzysztofwicki1@gmail.com>
2024-04-16 20:16:33 +0200

    feat: add clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,29 @@
 import { db } from "~/server/db";
+import { auth, currentUser } from "@clerk/nextjs";
 
 export const dynamic ="force-dynamic";
 
 export default async function HomePage() {
+  const { userId } = auth();
+  const user = await currentUser()
+  console.log(user)
 
   const bingoCards = await db.query.bingoCards.findMany();
 
   console.log(bingoCards)
 
   return (
-    <main className="flex min-h-screen flex-col items-center mt-4">
-      {bingoCards.map((bingoCard) => (
-        <div key={bingoCard.id} className="p-4 border rounded-lg mt-2">
-          <h2>{bingoCard.name}</h2>
-        </div>
-      ))}
-    </main>
+    <>
+      
+      <main className="flex min-h-screen flex-col items-center mt-4">
+        {bingoCards.map((bingoCard) => (
+          <div key={bingoCard.id} className="p-4 border rounded-lg mt-2">
+            <h2>{bingoCard.name}</h2>
+          </div>
+        ))}
+      </main>
+
+    </>
+
   );
 }
--- src/components/ui/Navbar/index.tsx
+++ src/components/ui/Navbar/index.tsx
@@ -1,8 +1,15 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between w-full p-4 text-xl fint-semibold border-b">
       <div>Bingo</div>
-      <div>Sign in</div>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </nav>
   );
 }
