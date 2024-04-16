55ae47822acfe0ff459f42c96d24a646e2bfc5d7
unknown <krzysiekwicki1@gmail.com>
2024-04-16 19:33:01 +0200

    feat: update libs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { db } from "~/server/db";
 
+export const dynamic ="force-dynamic";
+
 export default async function HomePage() {
 
   const posts = await db.query.posts.findMany();
@@ -9,7 +11,7 @@ export default async function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center mt-4">
       {posts.map((post) => (
-        <div key={post.id} className="p-4 border rounded-lg">
+        <div key={post.id} className="p-4 border rounded-lg mt-2">
           <h2>{post.name}</h2>
           <p>{post.createdAt.toString()}</p>
         </div>
