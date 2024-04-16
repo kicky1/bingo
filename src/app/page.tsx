bc53da924fcd2fac74644da1d94f9495117001c5
unknown <krzysztofwicki1@gmail.com>
2024-04-16 20:30:05 +0200

    feat: prepare bingo cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,28 @@
-import { db } from "~/server/db";
-import { auth, currentUser } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
+import { Card, CardContent, CardHeader } from "~/components/ui/card";
+import { getBingoCards } from "~/server/queries";
 
 export const dynamic ="force-dynamic";
 
 export default async function HomePage() {
-  const { userId } = auth();
   const user = await currentUser()
   console.log(user)
-
-  const bingoCards = await db.query.bingoCards.findMany();
-
-  console.log(bingoCards)
-
+  const bingoCards = await getBingoCards();
   return (
     <>
-      
       <main className="flex min-h-screen flex-col items-center mt-4">
+      <div className="h-fit w-fit p-4 mt-8 border-2 border-slate-200 bg-slate-50 rounded">
+          <div className="grid grid-cols-3 gap-4 sm:grid-cols-4">
         {bingoCards.map((bingoCard) => (
-          <div key={bingoCard.id} className="p-4 border rounded-lg mt-2">
-            <h2>{bingoCard.name}</h2>
-          </div>
+          <Card key={bingoCard.id}>
+            <CardHeader>
+              {bingoCard.name}
+            </CardHeader>
+          </Card>
         ))}
+        </div>
+        </div>
       </main>
-
     </>
-
   );
 }
