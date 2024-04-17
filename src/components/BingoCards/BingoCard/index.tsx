aaef6919cb40f2d8f5a22569530ffd862ec73fc9
unknown <krzysztofwicki1@gmail.com>
2024-04-17 16:56:45 +0200

    feat: update put method for cards

---
+++ src/components/BingoCards/BingoCard/index.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import api from "~/utils/api";
+import { getQueryClient } from "~/lib/query";
+import { Card, CardHeader } from "~/components/ui/card";
+
+export default function BingoCard({card} : any) {
+    const queryClient = getQueryClient()
+
+    const mutation = useMutation({
+        mutationFn: (card: any) => {
+          return api.put('/bingoCards', JSON.stringify({ id: card.id, checked: !card.checked }))
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['bingocards-data']})
+        }
+    })
+
+    return (
+        <>
+            <Card
+                onClick={() => mutation.mutate(card)}  
+                style={{backgroundColor: card?.checked ? 'green' : 'white'}}
+                key={card?.id}
+            >
+                <CardHeader>
+                    {card?.name}
+                </CardHeader>
+            </Card>
+        </>
+    );
+}
--- src/components/BingoCards/index.tsx
+++ src/components/BingoCards/index.tsx
@@ -1,41 +1,17 @@
 "use client";
 
-import { useMutation } from "@tanstack/react-query";
-import { Card, CardHeader } from "../ui/card";
-import api from "~/utils/api";
-import { getQueryClient } from "~/lib/query";
 import { useGetUsersChoice } from "~/actions/get-bingocards";
-
-
+import BingoCard from "./BingoCard";
 
 export default function BingoCards() {
-    const queryClient = getQueryClient()
-
     const {data, isLoading} = useGetUsersChoice()
-
-    const mutation = useMutation({
-        mutationFn: (card: any) => {
-          return api.put('/bingoCards', JSON.stringify({ id: card.id, checked: !card.checked }))
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries({queryKey: ['bingocards-data']})
-        }
-    })
-
+   
     if (isLoading) return <div>Loading...</div>
- 
+
     return (
         <>
-            {data?.map((card: any) => (
-                <Card
-                    onClick={() => mutation.mutate(card)}  
-                    style={{backgroundColor: card?.checked ? 'green' : 'slate'}}
-                    key={card?.id}
-                >
-                    <CardHeader>
-                        {card?.name}
-                    </CardHeader>
-                </Card>
+            {data.map((card: any) => (
+                <BingoCard card={card} key={card.id} />
             ))}
         </>
     );
