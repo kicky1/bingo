da620161c9eb691abfb188625122543cd1fb60d0
unknown <krzysztofwicki1@gmail.com>
2024-04-20 22:10:24 +0200

    feat: prepare new users table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import BingoCards from "~/components/BingoCards";
+import { UsersDialog } from "~/components/Dialogs/UsersDialog";
 export const dynamic ="force-dynamic";
 
 export default async function HomePage() {
   return (
     <>
       <main className="flex flex-col items-center mt-4">
+        <UsersDialog/>
         <div className="h-fit w-fit p-4 mt-8 border-2 border-slate-200 bg-slate-50 rounded m-4">
           <BingoCards/>
         </div>
