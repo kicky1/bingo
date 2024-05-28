7e3254dfda266b45464b29a04714df7983cb5e1f
unknown <krzysztofwicki1@gmail.com>
2024-05-28 22:03:35 +0200

    feat: delete cron

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-// import { authMiddleware } from "@clerk/nextjs";
-
-// export default authMiddleware({
-//   publicRoutes: ["/api/cron"],
-// });
-
-export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
